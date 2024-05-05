d341b9b361f773e504ef09d14c6c0b3a4de9a596
Eko Muchamad Haryono <66320809+ekomh170@users.noreply.github.com>
2024-05-05 15:22:37 +0700

    25. Menambahkan Data Master Users Untuk Admin Membuat Akun Users Pengguna Aplikasi Puskesmas

---
+++ menu/users/index.php
@@ -0,0 +1,100 @@
+<?php
+require_once 'header.php';
+require_once 'sidebar.php';
+?>
+
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+    <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Menu Data Akun Aplikasi Puskesmas</h1>
+                </div>
+            </div>
+        </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+
+        <div class="container-fluid">
+            <div class="row justify-content-center">
+                <div class="col-12">
+                    <!-- Default box -->
+                    <div class="card">
+                        <div class="card-header">
+                            <h3 class="card-title">Tabel Akun Aplikasi Puskesmas</h3>
+
+                            <div class="card-tools">
+                                <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+                                    <i class="fas fa-minus"></i>
+                                </button>
+                                <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
+                                    <i class="fas fa-times"></i>
+                                </button>
+                            </div>
+                        </div>
+                        <div class="card-body table-responsive">
+                            <h2 class="text-center">Data Akun Aplikasi Puskesmas</h2>
+                            <a href="add.php"><button class="btn btn-primary mb-1">Tambah Data</button></a>
+                            <table class="table table-striped text-center">
+                                <thead>
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>Nama Panggilan</th>
+                                        <th>Nama Lengkap</th>
+                                        <th>Email</th>
+                                        <th>Foto</th>
+                                        <th>Role</th>
+                                        <th>Waktu Buat</th>
+                                        <th>Aksi</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php
+                                    // Panggil file
+                                    require '../../config/dbkoneksi.php';
+                                    // Bikin Query SQL
+                                    $query = $dbh->query("SELECT * FROM users");
+                                    // Tampilkan data looping
+                                    foreach ($query as $row) {
+                                        echo "<tr>";
+                                        echo "<td>" . $row['user_id'] . "</td>";
+                                        echo "<td>" . $row['fullname'] . "</td>";
+                                        echo "<td>" . $row['username'] . "</td>";
+                                        echo "<td>" . $row['email'] . "</td>";
+                                        echo "<td>" . $row['foto'] . "</td>";
+                                        echo "<td>" . $row['role'] . "</td>";
+                                        echo "<td>" . $row['created_at'] . "</td>";
+                                        echo "<td>";
+                                        echo "<a href='edit.php?id=" . $row['user_id'] . "'><button class='btn btn-warning'>Edit</button></a>";
+                                        echo "<span class='mx-1'></span>"; 
+                                        echo "<a href='delete.php?id=" . $row['user_id'] . "' onclick='return confirm(\"Yakin Hapus Data?\")'><button class='btn btn-danger'>Delete</button></a>";
+                                        echo "</td>";
+                                        echo "</tr>";
+                                    }
+                                    ?>
+                                </tbody>
+                            </table>
+                        </div>
+                        <!-- /.card-body -->
+                        <div class="card-footer">
+                            Project 1 - Aplikasi CRUD Sederhana Puskesmas
+                        </div>
+                        <!-- /.card-footer-->
+                    </div>
+                    <!-- /.card -->
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- /.content -->
+</div>
+
+<!-- /.content-wrapper -->
+
+<?php
+require_once 'footer.php';
+?>
